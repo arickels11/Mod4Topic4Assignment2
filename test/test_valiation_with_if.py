5c5aa76a7462a687e5972a04bf1d1b55217e0780
arickels11 <arickels93@gmail.com>
2019-09-16 20:09:51 -0500

    All Tests Pass

---
+++ test/test_valiation_with_if.py
@@ -0,0 +1,12 @@
+from input_validation import validation_with_if
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_input_validation_if(self):
+        self.assertEqual(-1, validation_with_if.average(-90, 90, 95))
+
+
+if __name__ == '__main__':
+    unittest.main()
