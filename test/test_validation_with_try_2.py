db4501d4e06ed92d62030c1ea5aec77500adaead
arickels11 <arickels93@gmail.com>
2019-09-16 21:20:55 -0500

    test 1 pass

--- test/test_validation_with_try_2.py
+++ test/test_validation_with_try_2.py
@@ -1,12 +1,13 @@
-from input_validation import validation_with_try
+from input_validation import validation_with_try_2
 import unittest
 
 
 class MyTestCase(unittest.TestCase):
     def test_average_exception(self):
         with self.assertRaises(ValueError):
-            validation_with_try.average(-90, 60, 89)
+            validation_with_try_2.average(-90, 60, 89)
 
 
 if __name__ == '__main__':
     unittest.main()
+# to test value errors
