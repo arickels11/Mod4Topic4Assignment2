dab3f9fe64561e7870a3c6fd4d9e8a5872180708
arickels11 <arickels93@gmail.com>
2019-09-16 20:47:25 -0500

    Initial Commit

--- test/test_valiation_with_if.py
+++ test/test_valiation_with_if.py
@@ -5,7 +5,7 @@ import unittest
 class MyTestCase(unittest.TestCase):
 
     def test_input_validation_with_if(self):
-        self.assertEqual(-1, validation_with_if.average(90, 90, -95))
+        self.assertEqual(-1, validation_with_if.average(-90, -90, -95))
 
 
 if __name__ == '__main__':
