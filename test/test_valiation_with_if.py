f0a699930064b24b64abddd97998219de2acde55
arickels11 <arickels93@gmail.com>
2019-09-16 20:11:31 -0500

    1st commit - Test fail

--- test/test_valiation_with_if.py
+++ test/test_valiation_with_if.py
@@ -4,7 +4,7 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
 
-    def test_input_validation_if(self):
+    def test_input_validation_with_if(self):
         self.assertEqual(-1, validation_with_if.average(-90, 90, 95))
 
 
